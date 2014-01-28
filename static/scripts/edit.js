7a8269a4c1c5f17d4a544f37e6cc7ca2b0ca6d53
Josh Andrews <joshphilipandrews@gmail.com>
2014-01-28 01:11:34 -0800

    updated medium editor and references to jquery

--- static/scripts/edit.js
+++ static/scripts/edit.js
@@ -21,5 +21,5 @@ $( document ).ready(function() {
         $('#published-input').val("0");
     });
 	//https://github.com/orthes/medium-editor-images-plugin
-	//$('.editable').mediumImages();
+	$('.editable').mediumImages();
 });
